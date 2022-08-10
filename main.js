cf613a5b3c407cbdafa33dfa31be1686151a3f92
Gerald Akinola <akinolagerald@gmail.com>
2022-08-10 15:12:52 -0700

    console.log explanation

--- main.js
+++ main.js
@@ -17,10 +17,37 @@ const todos = [
  
     {
         id: 3,
-        text: 'Dentist appt',
+        text: `Dentist appt`,
         isCompleted: false
     }
 ];
 
+
+const myname = 'bola'
+
+
+
+// for 
+
 const todoJSON = JSON.stringify(todos);
 console.log(todos);
+
+console.log('My name is ' + myname + ' and i am 6 years old')
+
+//console.log(`My name is ${myname} and i am 6 years old` ${todos[0].text})
+
+console.log(console)
+
+const person = {}
+person.name= 'Tope'
+person.age= 21
+
+console.log(person)
+
+console.log(todos[0].text)
+
+const callmyname = function(name){
+    console.log(`Welcome ${name}`)
+}
+
+callmname('Tope')
