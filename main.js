7048b84fc93339ebcf43f4f45b983fa4f432033d
Gerald Akinola <akinolagerald@gmail.com>
2022-08-10 07:41:41 -0700

    use of arrays in JS

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-const s = 'yam, beans, stew, egg'
+const person = {
+    firstname: 'Gerald',
+    lastname: 'Akinola',
+    age: 30,
+    hobbies: ['music', 'movies', 'sleep'],
+    address: {
+        street: '30 main st',
+        city: 'boston', 
+        state: 'MA'
+    }
+   }
 
-console.log(s.split(', '));
+   console.log(person.hobbies[1]);
