7cc91fbc8ed48771023eb83fb6fc586ab956529c
Gerald Akinola <akinolagerald@gmail.com>
2022-08-15 05:32:58 -0700

    javascription continuation

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
+const fruits = ['orange', 'pineapple','paw-paw', 'cocoa']
+
+fruits[4] = 'mango'
+
+
+fruits.push('mangos')
 
 
 
-const names= ['Bolas', 'Topesss', 'Sades', 'Temisss']
-
-
-// for(let i = 0; i < names.length; i++){
-//    console.log(names[i])
-// }
-
-
-names.forEach(person=>{
-    console.log(`Welcome to my party ${person}`)
-})
-
-const namestoprint =names.filter(person=>{
-    return person.length===4
-}
-    
-    )
-
-    console.log(namestoprint)
+console.log(fruits);
