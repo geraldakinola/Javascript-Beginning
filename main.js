17ee2f41bd4174c8526e09c5c1305aa76bf805b0
Gerald Akinola <akinolagerald@gmail.com>
2022-08-16 04:17:02 -0700

    tenary operator explained

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-const x = 20
-const color = x>10 ? 'red' : 'green';
+const x = 9; 
+const color = 'green';
 
 console.log(color)
+switch(color){
+  case 'red':
+  console.log('color is red');
+  break;
+  case 'blue':
+  console.log('color is green')
+  break;
+ default:
+  console.log ('color is NOT red or green');
+  break;
+  
+
+}
