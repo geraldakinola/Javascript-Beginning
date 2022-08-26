5d7dbd15087841cb60e24473606ccfe621224bc7
Gerald Akinola <akinolagerald@gmail.com>
2022-08-26 05:49:22 -0700

    Destructuring Syntax

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const person = {
-    name: 'Max'
-};
-const newPerson = {
-    ...person,
-    age: 28,
-}
-console.log(newPerson);
+
+
+
+// const names= ['Bolas', 'Topesss', 'Sades', 'Temisss']
+
+
+// // for(let i = 0; i < names.length; i++){
+// //    console.log(names[i])
+// // }
+
+
+// names.forEach(person=>{
+//     console.log(`Welcome to my party ${person}`)
+// })
+
+// const namestoprint =names.filter(person=>{
+//     return person.length===4
+// }
+
+//     )
+
+//     console.log(namestoprint)
+
+// Spread and Rest Operators
+// const numbers = [1, 2, 3, 4];
+// const newNumbers = [... numbers, 5]
+
+// // console.log(newNumbers);
+
+// Destructing Syntax
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+console.log(num1, num2);
