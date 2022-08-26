ed2854e0fc4f1ef8254f535ace3b1b715b279a34
Gerald Akinola <akinolagerald@gmail.com>
2022-08-26 13:40:02 -0700

    Still on Arrays

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-// reference and primitive types 
-const person = {
-    name: 'Max'
-};
-const secondPerson = { ...person }
-person.name = 'Manu';
-
-console.log(secondPerson);
+const numbers = [1, 2, 3];
+const doubleNumArray = numbers.map((num) => { return num * 2 }
+);
+console.log(numbers)
+console.log(doubleNumArray)
