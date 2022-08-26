21682e9eb94f3937c720d55ca887fc85beecaadb
Gerald Akinola <akinolagerald@gmail.com>
2022-08-26 05:56:16 -0700

    referencce and primitive types

--- main.js
+++ main.js
@@ -1,33 +1,4 @@
-
-
-
-// const names= ['Bolas', 'Topesss', 'Sades', 'Temisss']
-
-
-// // for(let i = 0; i < names.length; i++){
-// //    console.log(names[i])
-// // }
-
-
-// names.forEach(person=>{
-//     console.log(`Welcome to my party ${person}`)
-// })
-
-// const namestoprint =names.filter(person=>{
-//     return person.length===4
-// }
-
-//     )
-
-//     console.log(namestoprint)
-
-// Spread and Rest Operators
-// const numbers = [1, 2, 3, 4];
-// const newNumbers = [... numbers, 5]
-
-// // console.log(newNumbers);
-
-// Destructing Syntax
-const numbers = [1, 2, 3];
-[num1, num2] = numbers;
-console.log(num1, num2);
+// reference and primitive types 
+const number = 1;
+const num1 = number;
+console.log(num1);
